0e83869f04e8fd10f2548855bf0984a5151548b4
Gaddam Bhavyasri <89065809+gaddambhavyasri@users.noreply.github.com>
2021-12-21 13:21:01 +0530

    Add files via upload

---
+++ QUIZZ APP USING JS/js/question.js
@@ -0,0 +1,64 @@
+// creating an array and passing the number,questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "HTML stands for -",
+        answer: "HyperText Markup Language",
+        options: [
+            "HighText Machine Language",
+            "HyperText and links Markup Language",
+            "HyperText Markup Language",
+            "None of these"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "The correct sequence of HTML tags for starting a webpage is -",
+        answer: "HTML, head, Title, body",
+        options: [
+            "Head, Title, HTML, body",
+            "HTML, Body, head, title",
+            "HTML, head, Title, body",
+            "HTML, Head, Title, Body"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "Input tag is -",
+        answer: "an empty tag.",
+        options: [
+            "a format tag.",
+            "an empty tag.",
+            "All of the above",
+            "None of the above"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "line-break tag in HTML?",
+        answer: "Break tag",
+        options: [
+            "Break tag",
+            "Anchor tag",
+            "Formatted tag",
+            "Bold tag"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "Ordered list tag in HTML?",
+        answer: "Ordered list tag",
+        options: [
+            "Unorder list tag",
+            "Ordered list tag",
+            "List tag",
+            "Italic tag"
+        ]
+    },
+
+    
+];
